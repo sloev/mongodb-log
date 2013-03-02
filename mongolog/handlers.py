715fb53e57b90567c55b1cbe86a8c966c66dec24
lilydjwg <lilydjwg@gmail.com>
2013-03-02 19:09:30 +0800

    don't overwrite record.msg

--- mongolog/handlers.py
+++ mongolog/handlers.py
@@ -19,13 +19,15 @@ class MongoFormatter(logging.Formatter):
         data = record.__dict__.copy()
 
         if record.args:
-            record.msg = record.msg % record.args
+            msg = record.msg % record.args
+        else:
+            msg = record.msg
 
         data.update(
             username=getpass.getuser(),
             time=datetime.now(),
             host=gethostname(),
-            message=record.msg,
+            message=msg,
             args=tuple(unicode(arg) for arg in record.args)
         )
         if 'exc_info' in data and data['exc_info']:
